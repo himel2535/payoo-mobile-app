4c2e4ff57efb853986207df482d828cd221338c2
himel2535 <monwarhossanhimel@gmail.com>
2025-08-24 00:38:30 +0600

    Login Functionality & Redirect to Home

---
+++ script.js
@@ -0,0 +1,20 @@
+// log in button functionality
+
+document.getElementById('loginButton').addEventListener('click',function(event){
+    event.preventDefault()
+    const mobileNumber=1234567890;
+    const pinNumber=1234;
+    const mobileNumberValue=document.getElementById('mobile-number').value
+    const mobileNumberValueConverted=parseInt(mobileNumberValue);
+
+    const pinNumberValue=document.getElementById('pin-number').value;
+    const pinNumberValueConverted=parseInt(pinNumberValue);
+    // console.log(mobileNumberValueConverted,pinNumberValueConverted)
+
+    if(mobileNumberValueConverted===mobileNumber && pinNumberValueConverted===pinNumber){
+        window.location.href="/home.html"
+    }
+    else{
+        alert("Invalid credentials")
+    }
+})
